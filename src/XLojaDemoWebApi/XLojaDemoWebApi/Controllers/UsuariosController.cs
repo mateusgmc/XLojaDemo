1665ab90964d3beb02858691584d0c658789e218
Mateus Mauricio <mateus10.m@live.com>
2019-12-06 06:17:55 -0300

    Adição de métodos ao controller

--- src/XLojaDemoWebApi/XLojaDemoWebApi/Controllers/UsuariosController.cs
+++ src/XLojaDemoWebApi/XLojaDemoWebApi/Controllers/UsuariosController.cs
@@ -21,5 +21,23 @@ namespace XLojaDemoWebApi.Controllers
         {
             return _usuarioRepository.GetAll();
         }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _usuarioRepository.Delete(x => x.Id == id);
+        }
+
+        [HttpPost]
+        public void Add(Usuario usuario)
+        {
+            _usuarioRepository.Add(usuario);
+        }
+
+        [HttpPut]
+        public void AddOrUpdate(Usuario usuario)
+        {
+            _usuarioRepository.AddOrUpdate(usuario);
+        }
     }
 }
